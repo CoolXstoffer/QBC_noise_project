64477dd9bbfac40edb4f8dcd5dc86db0e541335a
Christoffer Grauballe // s234801 <s234801@dtu.dk>
2025-03-28 13:19:34 +0100

    updated config comments

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ INITIAL_LABELED_SIZE = 50  # Number of initially labeled samples
 NOISE_PERCENTAGES = [0,5,10,15,20] # Percentages of label noise to test
 BATCH_SIZE = 10  # Number of samples to query in each active learning iteration
 NUM_ITERATIONS = 70#50  # Number of active learning iterations
-TRAIN_SIZE = 1500
-TEST_SIZE = 500
+TRAIN_SIZE = 1500 # Size of training data subset
+TEST_SIZE = 500 # SIze of testing data subset
 
 # Model parameters
 HIDDEN_LAYER_SIZES = (50, 50)  # Architecture for the base neural network
